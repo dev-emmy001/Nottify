cc4f09b9c651a6af5d8be9a9bbd71f0abc6eeadb
devemmy001 <devemmy001@gmail.com>
2025-05-22 17:55:47 +0100

    Fixed some dependency issues

--- nottify/lib/main.dart
+++ nottify/lib/main.dart
@@ -9,54 +9,21 @@ void main() {
           seedColor: const Color.fromARGB(255, 103, 58, 183),
         ),
       ),
-      home: const MyHomePage(title: 'Nottify'),
+      home: const HomePage(),
     ),
   );
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
+      appBar: AppBar(title: const Text("Welcome to Nottify")),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('Take notes comfortably with nottify'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+        child: TextButton(onPressed: () {}, child: const Text("Register")),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
